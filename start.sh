6f5fa95e7c943fb88d803353c4cd42c97233da34
HDCinema4U <115459136+HDCinema4U@users.noreply.github.com>
2023-04-25 17:49:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  gh repo clone matrixbotz0/PETER-PARKER-BOT
+  gh repo clone HDCinema4U/imdb
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PETER-PARKER-BOT
 fi
 cd /PETER-PARKER-BOT
 pip3 install -U -r requirements.txt
-echo "Starting Bot..."
+echo "Starting Bot IMDB..."
 python3 bot.py
