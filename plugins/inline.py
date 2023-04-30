57b9b1c7be049cf6a29a908843e727144485417a
techhm <115459136+techhm@users.noreply.github.com>
2023-04-30 14:07:59 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🔎 sᴇᴀʀᴄʜ ᴀɢᴀɪɴ 🔎', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('🔎 Sᴇᴀʀᴄʜ Aɢᴀɪɴ 🔍', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
