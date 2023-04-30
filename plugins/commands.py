f71baab569283a2920ece89b11588a1b1529ab76
techhm <115459136+techhm@users.noreply.github.com>
2023-04-30 13:17:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton('sᴜʀᴘʀɪsᴇ', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIRBGPwcjyu8rC9ISXtapMGNc9VxFvOAAInAQACyJRkFOI9YoCRRKaaHgQ") 
+        m=await message.reply_text('ɯαιƚ Ⴆɾσ') 
         await asyncio.sleep(1)
         await m.delete()        
         await message.reply_photo(
