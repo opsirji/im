36d81824745942e5610d6b055a50a516271807c7
techhm <115459136+techhm@users.noreply.github.com>
2023-05-09 21:23:53 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -39,7 +39,7 @@ async def index_files(bot, query):
     await msg.edit(
         "Starting Indexing",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
+            [[InlineKeyboardButton('⛔️ ᴄᴀɴᴄᴇʟ ⛔️', callback_data='index_cancel')]]
         )
     )
     try:
